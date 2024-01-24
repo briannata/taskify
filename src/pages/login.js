b0554064ba6fa490be2610ac6c450c84e2d6de84
briannata <brianna.hy.ta@gmail.com>
2024-01-24 10:52:12 -0500

    Add unique username checking

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,34 +2,42 @@ import { useState } from 'react';
 import '../App.css';
 import { db } from '../firebase-config';
 import { doc, getDoc } from '@firebase/firestore';
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [username, setUsername] = useState("");
+  const [usernameError, setUsernameError] = useState("")
+  const navigate = useNavigate();
 
-  const LoginUser = async () => {
+  const onFormSubmit = async (e) => {
+    e.preventDefault();
     const docRef = doc(db, "users", username);
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
-    console.log("Document data:", docSnap.data());
+        navigate("/" + username);
     } else {
-    // docSnap.data() will be undefined in this case
-    console.log("No such document!");
+        setUsernameError("Username does not exist. Please register first.")
     }
-    //window.location.reload();
   };
  
   return (
-    <div className='text-white'>
-        <h1 className=' w-screen text-center mt-8 text-4xl font-bold'>Taskify</h1>
-        <p className='w-screen text-center mt-5'>Login</p>
-        <div className='text-center mt-16'>
-            <span>Enter your Username : </span>
-            <input className='mx-4 text-black' type="text" placeholder='Username' onChange={(event) => { setUsername(event.target.value)}} />
-            <br />
-            <button onClick={LoginUser} className='bg-slate-700 m-4 p-2 w-20 rounded-md'>Login</button>
-            <p>Haven't logged in yet? Register <a href="./register">here</a></p>
+    <div className='text-white mainContainer'>
+        <div className='titleContainer'>
+            <div className=' w-screen text-center mt-8 text-4xl font-bold'>Taskify Login</div>
         </div>
+        <br />
+        <br />
+        <form className='text-center mt-16' onSubmit={onFormSubmit}>
+            <div className='inputContainer'>
+                <input className='mx-4 text-black inputBox' type="text" placeholder='Enter your username here' onChange={(event) => { setUsername(event.target.value)}} />
+                <label className="errorLabel">{usernameError}</label>
+            </div>
+            <div className='buttonContainer'>
+                <button type="submit" className='bg-slate-700 m-4 p-2 w-20 rounded-md'>Login</button>
+                <label className="errorLabel">Haven't logged in yet? Register <a href="./register">here</a></label>
+            </div>
+        </form>
     </div>
     
   );
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,30 +1,63 @@
 import { useState } from 'react';
 import '../App.css';
 import { db } from '../firebase-config';
-import { setDoc, doc } from '@firebase/firestore';
+import { setDoc, doc, getDoc } from '@firebase/firestore';
+import { useNavigate } from "react-router-dom";
 
 function Register() {
   const [username, setUsername] = useState("");
   const [name, setName] = useState("");
+  const [usernameError, setUsernameError] = useState("")
+  const navigate = useNavigate();
 
-  const CreateUser = async () => {
-    await setDoc(doc(db, "users", username), { Username: username, Name: name });
-    window.location.reload()
+  const userExists = async () => {
+    const docRef = doc(db, "users", username);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        return true;
+    } else {
+        setUsernameError("Username already exists. Please try a different username.")
+        return false;
+    }
+  }
+
+  const onFormSubmit = async (e) => {
+    e.preventDefault();
+
+    const docRef = doc(db, "users", username);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      setUsernameError("Username already exists. Please try a different username.");
+    } else {
+      await setDoc(doc(db, "users", username), { Username: username, Name: name });
+      navigate("/" + username);
+    }
+
   };
  
   return (
-    <div className='text-white'>
-        <h1 className=' w-screen text-center mt-8 text-4xl font-bold'>Taskify</h1>
-        <p className='w-screen text-center mt-5'>Registration</p>
-        <div className='text-center mt-16'>
-            <span>Enter your Username : </span>
-            <input className='mx-4 text-black' type="text" placeholder='Username' onChange={(event) => { setUsername(event.target.value)}} />
-            <br />
-            <span>Enter your Name : </span>
-            <input className='mx-4 text-black' type="text" placeholder='Name' onChange={(event) => { setName(event.target.value)}} />
-            <br />
-            <button onClick={CreateUser} className='bg-slate-700 m-4 p-2 w-20 rounded-md'>Register</button>
+    <div className='text-white mainContainer'>
+        <div className='titleContainer'>
+            <div className=' w-screen text-center mt-8 text-4xl font-bold'>Taskify Registration</div>
         </div>
+        <br />
+        <br />
+        <form className='text-center mt-16' onSubmit={onFormSubmit}>
+        <div className='inputContainer'>
+                <input className='mx-4 text-black inputBox' type="text" placeholder='Enter your username here' onChange={(event) => { setUsername(event.target.value)}} />
+                <label className="errorLabel">{usernameError}</label>
+            </div>
+            <br />
+            <div className='inputContainer'>
+            <input className='mx-4 text-black inputBox' type="text" placeholder='Enter your name here' onChange={(event) => { setName(event.target.value)}} />
+            </div>
+            <br />
+            <div className='buttonContainer'>
+                <button type="submit" className='bg-slate-700 m-4 p-2 w-20 rounded-md'>Login</button>
+            </div>
+        </form>
     </div>
     
   );
